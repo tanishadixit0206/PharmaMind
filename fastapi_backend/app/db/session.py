16940754522c904b82633cdda78bfb8e99f0b8aa
Tanisha Dixit <dixittanisha02@gmail.com>
2025-02-26 09:55:44 +0530

    chore: modifies db configuration

--- fastapi_backend/app/db/session.py
+++ fastapi_backend/app/db/session.py
@@ -2,6 +2,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.core.config import MONGO_URI
 
 client = AsyncIOMotorClient(MONGO_URI)
-db = client["pharmacy"]
+db = client["PharmaMindDatabase"]
 users_collection = db["users"]
 prescriptions_collection = db["prescriptions"]
